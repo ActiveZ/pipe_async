59b91ff7696610f009eaa7b2c05dffcf288aa70b
ActiveZ <activez.git@gmail.com>
2023-12-23 01:37:54 +0100

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AppService } from './services/app.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -25,12 +25,12 @@ export class AppComponent {
     this.listNumbers$ = undefined;
     this.listNumbers$ = this.appService.listNumbers$.asObservable();
     this.appService.listNumbers$.asObservable().subscribe({ // affiche les mêmes données dans la console, recrée une nouvelle instance ?
-      next: (data) => console.log('data', data),
+      next: (data: any) => console.log('data', data),
     });
     
     // v2
     this.appService.data$.subscribe({
-      next: (newData) => this.data = newData
+      next: (newData: number[] | undefined) => this.data = newData
     });
     this.data = undefined;
   }
